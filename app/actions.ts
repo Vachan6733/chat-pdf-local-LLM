1b36bdfa9441b7be1f7215257a4b138425d76a8c
Roshan <rodavid20@users.noreply.github.com>
2024-09-26 14:16:34 +0530

    modified the topk and topp value to reduce probability of generating nonsense

--- app/actions.ts
+++ app/actions.ts
@@ -1,47 +1,55 @@
-'use server'
-import { VectorIndexRetriever, VectorStoreIndex } from "llamaindex/indices/vectorStore/index"
-import { ContextChatEngine } from "llamaindex/engines/chat/ContextChatEngine"
-import { OllamaEmbedding } from "llamaindex/embeddings/OllamaEmbedding"
-import { serviceContextFromDefaults } from "llamaindex/ServiceContext"
-import { Ollama } from "llamaindex/llm/ollama"
-import { SimpleDirectoryReader } from "llamaindex/readers/index"
-import { Metadata } from "next"
-import { Settings } from "llamaindex/Settings"
-import { BaseRetriever, Document } from "llamaindex"
+"use server";
+import {
+  VectorIndexRetriever,
+  VectorStoreIndex,
+} from "llamaindex/indices/vectorStore/index";
+import { ContextChatEngine } from "llamaindex/engines/chat/ContextChatEngine";
+import { OllamaEmbedding } from "llamaindex/embeddings/OllamaEmbedding";
+import { serviceContextFromDefaults } from "llamaindex/ServiceContext";
+import { Ollama } from "llamaindex/llm/ollama";
+import { SimpleDirectoryReader } from "llamaindex/readers/index";
+import { Metadata } from "next";
+import { Settings } from "llamaindex/Settings";
+import { BaseRetriever, Document } from "llamaindex";
 
-
+//set dummy key as we are not using openai
 process.env["OPENAI_API_KEY"] = "keyValue";
 interface LCDoc {
-  pageContent: string,
-  metadata: any,
+  pageContent: string;
+  metadata: any;
 }
 
 const embedModel = new OllamaEmbedding({
-  model: 'nomic-embed-text'
-})
+  model: "nomic-embed-text",
+});
 
 const llm = new Ollama({
   model: "phi",
   // model: "gemma",
   options: {
     temperature: 0,
-  }
-})
+    top_k: 0,
+    top_p: 0.5,
+  },
+});
 
 let chatEngine: ContextChatEngine | null = null;
 let retriever: BaseRetriever;
 
 export async function createIndex() {
   const lcDocs = await new SimpleDirectoryReader().loadData({
-    directoryPath: "./data"
+    directoryPath: "./data",
   });
 
-  const docs = lcDocs.map(lcDoc => new Document({
-    text: lcDoc.text,
-    metadata: lcDoc.metadata
-  }))
-  
-  Settings.llm = llm
+  const docs = lcDocs.map(
+    (lcDoc) =>
+      new Document({
+        text: lcDoc.text,
+        metadata: lcDoc.metadata,
+      })
+  );
+
+  Settings.llm = llm;
 
   // console.log(docs)
   //TODO: Store the index to a VectorStore and load from it
@@ -49,41 +57,41 @@ export async function createIndex() {
     serviceContext: serviceContextFromDefaults({
       chunkSize: 300,
       chunkOverlap: 20,
-      embedModel, llm
-    })
-  })
+      embedModel,
+      llm,
+    }),
+  });
 
   retriever = index.asRetriever({
     similarityTopK: 1,
-  })
+  });
 }
 
 export async function createChatEngineFromDocs() {
   if (chatEngine) {
-    chatEngine.reset()
+    chatEngine.reset();
   } else {
     await createIndex();
   }
   chatEngine = new ContextChatEngine({
     retriever,
-    chatModel: llm
-  })
+    chatModel: llm,
+  });
 }
-
 
 export async function chat(query: string) {
   if (chatEngine) {
     const queryResult = await chatEngine.chat({
-      message: query
-    })
-    const response = queryResult.response
-  
-    const metadata = queryResult.sourceNodes?.map(node => node.node.metadata)
+      message: query,
+    });
+    const response = queryResult.response;
+
+    const metadata = queryResult.sourceNodes?.map((node) => node.node.metadata);
     // const nodesText = queryResult.sourceNodes?.map(node => node.getContent(MetadataMode.LLM))
     //console.log(metadata)
     return { response, metadata };
   }
-  return  {response: '', undefined}
+  return { response: "", undefined };
 }
 
 export async function resetChatEngine() {
--- components/Home.tsx
+++ components/Home.tsx
@@ -1,78 +1,79 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from "react"
-import ChatWindow from '@/components/ChatWindow';
+import { useState, useEffect } from "react";
+import ChatWindow from "@/components/ChatWindow";
 
 import { createChatEngineFromDocs, chat } from "@/app/actions";
 import { ChatMessage } from "@/components/ChatWindow";
 
-
 export default function HomePage() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [loadingMessage, setLoadingMessage] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState("");
 
-  const [messages, setMessages] = useState<ChatMessage[]>([])
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
 
   const startChat = async (input: string) => {
     let reference = "";
-    setLoadingMessage("Please wait...")
-    setIsLoading(true)
+    setLoadingMessage("Please wait...");
+    setIsLoading(true);
     try {
-      setMessages([...messages, { role: 'human', statement: input },])
+      setMessages([...messages, { role: "human", statement: input }]);
       const { response, metadata } = await chat(input);
       if (metadata != undefined && metadata.length > 0) {
-        reference = metadata[0].file_name;
+        reference =
+          "Reference: " +
+          metadata[0].file_name +
+          " (Page No. " +
+          metadata[0].page_number +
+          ")";
         //reference = metadata[0].file_path;
       }
-      setMessages(
-        [
-          ...messages,
-          { role: 'human', statement: input },
-          { role: 'ai', statement: response },
-          { role: 'ref', statement: "Reference: " + reference},
-        ]
-      )
+      setMessages([
+        ...messages,
+        { role: "human", statement: input },
+        { role: "ai", statement: response },
+        { role: "ref", statement: reference },
+      ]);
       // console.log(metadata)
-      
-      setLoadingMessage("Response Recieved from LLM")
-    } catch (e) {
-      console.log(e)
-      setLoadingMessage("Error generating response.")
-    } finally {
-      setIsLoading(false)
-    }
-  }
 
+      setLoadingMessage("Response Recieved from LLM");
+    } catch (e) {
+      console.log(e);
+      setLoadingMessage("Error generating response.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    setLoadingMessage("Loading Index from the PDFs")
+    setLoadingMessage("Loading Index from the PDFs");
     setIsLoading(true);
     const createChatEngineFromDocsAsync = async () => {
       try {
-        await createChatEngineFromDocs()
-        setLoadingMessage("Done creating Index from the PDF.")
+        await createChatEngineFromDocs();
+        setLoadingMessage("Done creating Index from the PDF.");
       } catch (e) {
-        console.log(e)
-        setLoadingMessage("Error while creating index")
+        console.log(e);
+        setLoadingMessage("Error while creating index");
       } finally {
         setIsLoading(false);
       }
-    }
-    createChatEngineFromDocsAsync()
+    };
+    createChatEngineFromDocsAsync();
     // console.log(selectedFile)
-  }, [])
+  }, []);
 
   return (
     <div>
-      <div className='flex justify-evenly gap-2 h-[90vh]'>
-          <ChatWindow
-            isLoading={isLoading}
-            loadingMessage={loadingMessage}
-            startChat={startChat}
-            messages={messages}
-            setMessages={setMessages}
-          />
-        </div>
+      <div className="flex justify-evenly gap-2 h-[90vh]">
+        <ChatWindow
+          isLoading={isLoading}
+          loadingMessage={loadingMessage}
+          startChat={startChat}
+          messages={messages}
+          setMessages={setMessages}
+        />
+      </div>
     </div>
-  )
+  );
 }
